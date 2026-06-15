a5dde90e70ad0e89de49cd3bafd429ea425e144e
AlvaroL <lavado.mejia.alvaro.elias@gmail.com>
2026-06-14 19:55:07 -0500

    feat: implementé la clase Libro con sus respectivos atributos

--- src/model/Libro.java
+++ src/model/Libro.java
@@ -1,4 +1,34 @@
 package model;
+public class Libro{
+    private String titulo, autor, editorial, idioma, genero;
+    private int anio;
 
-public class Libro {
+    public Libro (String titulo, String autor, String editorial, String idioma, String genero, int anio){
+        this.titulo= titulo;
+        this.autor= autor;
+        this.editorial= editorial;
+        this.idioma= idioma;
+        this.genero= genero;
+        this.anio= anio;
+    }
+
+    public String getTitulo(){
+        return titulo;
+    }
+    public String getAutor() {
+        return autor;
+    }
+    public String getEditorial() {
+        return editorial;
+    }
+    public String getIdioma() {
+        return idioma;
+    }
+    public String getGenero() {
+        return genero;
+    }
+    public int getAnio(){
+        return anio;
+    }
+
 }
